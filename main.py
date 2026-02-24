1c45de7ce267e9bc92f26e1f2db6792254384304
Hounsoulin Hercule <jhounsoulin@gmail.com>
2026-02-24 16:05:47 +0100

    Suppression produits + retrait suppression ventes

--- main.py
+++ main.py
@@ -388,18 +388,18 @@ def generate_invoice_pdf(invoice, sales, total):
     buffer.seek(0)
     return buffer
 
-# ------------------ DELETE SALE ------------------
+# ------------------ DELETE PRODUCT ------------------
 
-@app.post("/delete-sale/{sale_id}")
-def delete_sale(sale_id: int, request: Request, db: Session = Depends(get_db)):
+@app.post("/delete-product/{product_id}")
+def delete_product(product_id: int, request: Request, db: Session = Depends(get_db)):
     if "user" not in request.session:
         return RedirectResponse(url="/login", status_code=303)
-    sale = db.query(Sale).filter(Sale.id == sale_id).first()
-    if not sale:
-        raise HTTPException(status_code=404, detail="Vente introuvable")
-    db.delete(sale)
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Produit introuvable")
+    db.delete(product)
     db.commit()
-    return RedirectResponse(url="/sales-page", status_code=303)
+    return RedirectResponse(url="/products-page", status_code=303)
 
 # ------------------ REAPPROVISIONNEMENT ------------------
 
